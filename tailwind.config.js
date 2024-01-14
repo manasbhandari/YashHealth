4a4f423e66f047378c8f429082da26ebf16f306a
manasbhandari <21bit135@ietdavv.edu.in>
2024-01-14 11:42:29 +0530

    update source url

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ "./src/**/*.{js,jsx,ts,tsx}",],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      backgroundImage:{
-        'photo': 'url(https://drive.google.com/file/d/1oGmuwghyYGJX37sbYItectHrABMXDOFd/preview)'
+      backgroundImage: {
+      
+        'photo': "url('../src/Assets/Photo.jpeg')",
       }
     },
   },
   plugins: [],
-}
-
+};
